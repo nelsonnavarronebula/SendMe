21d9e21b2c2f78dc8aaeff4bc0f36b72bb817ac0
Nelson Mauricio <nelsonnavarro@tmtek.net>
2025-09-04 21:16:28 -0500

    feat: change color standard, add mdx

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,19 @@
 // @ts-check
 import { defineConfig } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
+import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()],
   },
+
+  redirects: {
+    "/": "/es",
+  },
+
+   integrations: [
+    mdx(),
+  ],
 });
